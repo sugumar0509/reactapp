616573fdbdd583aab9b4fa09e871f565a4807717
sugumar <99330884+sugumar0509@users.noreply.github.com>
2022-03-28 11:23:08 +0530

    upload

---
+++ build/precache-manifest.67628f2379d1d39f2918331d3438ea6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45799fc7786aafa240bf",
+    "url": "/static/css/main.a9eaff17.chunk.css"
+  },
+  {
+    "revision": "45799fc7786aafa240bf",
+    "url": "/static/js/main.45799fc7.chunk.js"
+  },
+  {
+    "revision": "5cd4a15baff8c898af16",
+    "url": "/static/js/1.5cd4a15b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "abfdac133e03f0abfa64602c4ff04b1b",
+    "url": "/static/media/profile.abfdac13.jpg"
+  },
+  {
+    "revision": "d843d0a46c28788903fcdc55b8e14f81",
+    "url": "/index.html"
+  }
+];
